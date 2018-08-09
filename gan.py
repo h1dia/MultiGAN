f718690f713d541a7d8ef3399464452e3644a169
h1dia <perditus.p@gmail.com>
2018-08-09 16:11:30 +0900

    refactor

--- gan.py
+++ gan.py
@@ -116,3 +116,52 @@ class GAN:
 
         data = self.g(inputs, training=True)
         return data
+
+
+class MultiGAN:
+    def __init__(self, batch_size=1000, z_dim=256):
+        self.batch_size = batch_size
+        self.z_dim = z_dim
+
+        self.g = Generator()
+        self.d = VanillaDiscriminator()
+
+        self.z = tf.random_uniform([self.batch_size, self.z_dim], minval=-1.0, maxval=1.0)
+
+    def forward(self, traindata):
+        generated = self.g(self.z, training=True)
+
+        g_outputs = self.d(generated, training=True, name='g')
+        t_outputs = self.d(traindata, training=True, name='t')
+
+        # loss function
+        # OLD : -log(g)
+        # g_loss = tf.reduce_mean(-(tf.log(g_outputs)))
+
+        # Proposed : min KL(d|g)
+        g_loss = tf.reduce_mean((tf.log(1 - g_outputs) - tf.log(g_outputs)))
+
+        # g_loss = tf.reduce_mean((tf.log(1 - g_outputs)))
+        d_loss = tf.reduce_mean(-(tf.log(t_outputs) + tf.log(1 - g_outputs)))
+
+        return {
+            self.g: g_loss,
+            self.d: d_loss,
+        }
+
+    def train(self, losses, learning_rate=0.0002, beta1=0.5):
+        g_opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+        d_opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+
+        g_opt_op = g_opt.minimize(losses[self.g], var_list=self.g.variables)
+        d_opt_op = d_opt.minimize(losses[self.d], var_list=self.d.variables)
+
+        with tf.control_dependencies([g_opt_op, d_opt_op]):
+            return tf.no_op(name='train')
+
+    def sample_data(self, inputs=None):
+        if inputs is None:
+            inputs = self.z
+
+        data = self.g(inputs, training=True)
+        return data
